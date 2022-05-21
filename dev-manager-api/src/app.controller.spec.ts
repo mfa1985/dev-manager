f372dfd15c179e99d7e1ec11d445cacdaf78eeeb
Altair <chikaum1985@gmail.com>
2022-05-21 10:41:25 -0300

    Pequena implementacao de teste

--- dev-manager-api/src/app.controller.spec.ts
+++ dev-manager-api/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Seja bem vindo ao Gerenciador de Desenvolvedores"', () => {
+      expect(appController.getHello()).toBe('Seja bem vindo ao Gerenciador de Desenvolvedores');
     });
   });
 });
